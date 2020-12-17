e3181a2634bde072aacfbb90b42ace5e7ed89d72
josefjebavy <email@josefjebavy.cz>
2020-12-17 15:44:42 +0100

    example of webserver with nginx as proxy

---
+++ nginx-as-proxy/www/index.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+echo "\n<br>HTTP_X_FORWARDED_PROTO:";
+var_dump($_SERVER['HTTP_X_FORWARDED_PROTO']);
+echo"\n<br> HTTP_X_FORWARDED_PORT:";
+var_dump($_SERVER['HTTP_X_FORWARDED_PORT']);
+echo"\n<br> REMOTE_ADDR:";
+var_dump( $_SERVER['REMOTE_ADDR']);
+echo"\n<br> HTTPS:";
+  var_dump($_SERVER['HTTPS']);
+echo"\n<br> HTTP_X_FORWARDED_HOST:";
+var_dump($_SERVER['HTTP_X_FORWARDED_HOST']);
+echo"\n<br> HTTP_X_FORWARDED_SERVER:";
+    var_dump($_SERVER['HTTP_X_FORWARDED_SERVER']);
+
+echo"\n<br><br>";
+echo"\n<br><br> SERVER:";
+var_dump($_SERVER);
+
+exit;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+App\Bootstrap::boot()
+	->createContainer()
+	->getByType(Nette\Application\Application::class)
+	->run();
