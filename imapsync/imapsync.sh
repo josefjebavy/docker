f83f48ae6413d2600ac3457bfb13b63f58a240c1
josefjebavy <email@josefjebavy.cz>
2021-05-15 16:59:21 +0200

    init project: automatic imap import

---
+++ imapsync/imapsync.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+. ./server.sh
+
+while IFS=, read -r user1 pass1 user2 pass2
+
+do
+imapsync \
+ --host1 ${hostname1} --user1 ${user1} --password1 ${pass1} \
+ --host2 ${hostname1} --user2 ${user2} --password2 ${pass2}
+
+if [ $? -ne 0 ] ; then
+	echo ERROR
+	exit 1;
+fi
+
+done < input.csv
+
+
+
+
---
+++ imapsync/server.example.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+hostname1="server1.example.com"
+hostname2="server2.example.com"
