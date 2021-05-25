b3fd40ea2dc7bfec9d40354dae23728773507fb2
josefjebavy <email@josefjebavy.cz>
2021-05-25 15:33:35 +0200

    fix variable name

--- imapsync/imapsync.sh
+++ imapsync/imapsync.sh
@@ -6,7 +6,7 @@ while IFS=, read -r user1 pass1 user2 pass2
 do
 imapsync \
  --host1 ${hostname1} --user1 ${user1} --password1 ${pass1} \
- --host2 ${hostname1} --user2 ${user2} --password2 ${pass2}
+ --host2 ${hostname2} --user2 ${user2} --password2 ${pass2}
 
 if [ $? -ne 0 ] ; then
 	echo ERROR
